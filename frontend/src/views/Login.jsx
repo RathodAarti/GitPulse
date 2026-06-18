d6ae7ffc9d90b397cb95ca6c3aa8af7b57f609c0
Aarti Rathod <agrathod0701@gmail.com>
2026-06-18 16:56:16 +0530

    fix(Login): remove unused useEffect import

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useNavigate, useLocation, Link } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import { AlertIcon, EyeIcon, EyeOffIcon, ArrowLeftIcon } from '../components/Icons'
